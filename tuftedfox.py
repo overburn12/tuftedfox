53f27f2d62e8a5af212e5f405b6fcb5703f7b783
overburn <overburn12@gmail.com>
2023-11-06 09:12:06 -0500

    updated page tracking

--- tuftedfox.py
+++ tuftedfox.py
@@ -1,6 +1,8 @@
 import subprocess, json, os, requests, random
 from flask import Flask, render_template, request, jsonify, abort, Response, g, send_from_directory
 from werkzeug.utils import safe_join
+from werkzeug.exceptions import NotFound
+from werkzeug.routing import RequestRedirect
 from datetime import datetime
 from dotenv import load_dotenv
 
@@ -14,23 +16,42 @@ load_dotenv()
 secret_password = os.getenv('SECRET_PASSWORD')
 app_start_time = int(datetime.utcnow().timestamp())
 page_hits = {}
+page_hits_invalid = {}
 
 #-------------------------------------------------------------------
 # functions 
 #-------------------------------------------------------------------
 
 def save_page_hits():
-    with open('data/page_hits.json', 'w') as f:
-        json.dump(page_hits, f)
+    try:
+        with open('data/page_hits.json', 'w') as f:
+            json.dump(page_hits, f)
+    except IOError as e:
+        print(f"An error occurred while saving valid page hits: {e}")
+
+    try:
+        with open('data/page_hits_invalid.json', 'w') as f:
+            json.dump(page_hits_invalid, f)
+    except IOError as e:
+        print(f"An error occurred while saving invalid page hits: {e}")
 
 def load_page_hits():
+    tmp_hits = {}
+    tmp_invalid = {}
     try:
         with open('data/page_hits.json', 'r') as f:
-            return json.load(f)
+            tmp_hits = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
-        return {}
+        tmp_hits = {}
+    try:
+        with open('data/page_hits_invalid.json', 'r') as f:
+            tmp_invalid = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        tmp_invalid = {}
+        
+    return tmp_hits, tmp_invalid
 
-page_hits = load_page_hits()
+page_hits, page_hits_invalid = load_page_hits()
 
 #-------------------------------------------------------------------
 # page count injection
@@ -38,12 +59,18 @@ page_hits = load_page_hits()
 
 @app.before_request
 def before_request():
-    global page_hits
+    global page_hits, page_hits_invalid
     page = request.path
-
-    # Track hits per page
-    page_hits[page] = page_hits.get(page, 0) + 1
-    save_page_hits()  # Save page hits as needed, this is not a permanent spot to do it
+    try:
+        # Try to match the request path to the URL map
+        app.url_map.bind('').match(page)
+        # If the above line doesn't raise an exception, the route is valid
+        page_hits[page] = page_hits.get(page, 0) + 1
+    except (NotFound, RequestRedirect):
+        # If the route is not found or a redirect, consider it invalid
+        page_hits_invalid[page] = page_hits_invalid.get(page, 0) + 1
+    finally:
+        save_page_hits()
 
 #-------------------------------------------------------------------
 # page routes
@@ -67,12 +94,18 @@ def update_server():
 
 @app.route('/count')
 def count_page():
-    # Sort page_hits by hits in descending order
+    # Sort the valid page hits
     sorted_page_hits = sorted(page_hits.items(), key=lambda item: item[1], reverse=True)
-    # Convert the sorted list of tuples back into a dictionary
     sorted_page_hits_dict = dict(sorted_page_hits)
-    # Pass the sorted dictionary to the template
-    return render_template('count.html', page_hits=sorted_page_hits_dict)
+
+    # Sort the invalid page hits
+    sorted_page_hits_invalid = sorted(page_hits_invalid.items(), key=lambda item: item[1], reverse=True)
+    sorted_page_hits_invalid_dict = dict(sorted_page_hits_invalid)
+
+    # Pass both dictionaries to the template
+    return render_template('count.html',
+                           page_hits=sorted_page_hits_dict,
+                           page_hits_invalid=sorted_page_hits_invalid_dict)
 
 @app.route('/order')
 def order_page():
@@ -80,24 +113,17 @@ def order_page():
 
 @app.route('/gallery')
 def gallery_page():
-    # Define the directories where the images and thumbnails are stored
     rugs_folder = 'img/rugs'
-    thumbnails_folder = 'img/thumbnails'
-
-    # Initialize an empty list to hold the image URLs and thumbnail URLs
+    thumbnails_folder = 'img/rugs/thumbnails'
     image_data = []
-
-    # Get the list of image file names and sort them alphabetically
     image_names = sorted(os.listdir(rugs_folder))
 
-    # Construct the URLs for each image and its thumbnail
     for image_name in image_names:
         if os.path.isfile(os.path.join(rugs_folder, image_name)):
-            image_url = f'/img/rugs/{image_name}'
-            thumbnail_url = f'/img/rugs/thumbnails/{image_name}'
+            image_url = f'{rugs_folder}/{image_name}'
+            thumbnail_url = f'{thumbnails_folder}/{image_name}'
             image_data.append((thumbnail_url, image_url))
 
-    # Pass the image data to the template
     return render_template('gallery.html', image_data=image_data)
 
 
@@ -118,21 +144,17 @@ def favicon():
 
 @app.route('/img/<folder>/<path:image_name>')
 def serve_image(folder, image_name):
-    # List of valid folders
     valid_folders = ['404', 'rugs', 'icons']
     
-    # Check if the provided folder is valid
     if folder not in valid_folders:
         abort(404)
 
     image_folder = safe_join('img', folder)
     safe_image_path = safe_join(image_folder, image_name)
     
-    if not os.path.isfile(safe_image_path):  # Check if the file exists and is a file
+    if not os.path.isfile(safe_image_path):  
         abort(404)
 
-    # Send the file from the directory, ensuring that the MIME type and other
-    # headers are handled correctly.
     return send_from_directory(image_folder, image_name)
 
 #-------------------------------------------------------------------

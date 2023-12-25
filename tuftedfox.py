7d83fdc3ac7c6f8e0500b07d695e91f85f92dfd3
overburn <overburn12@gmail.com>
2023-12-24 21:53:13 -0500

    removed the limiter uwu~

--- tuftedfox.py
+++ tuftedfox.py
@@ -17,11 +17,11 @@ from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 
-limiter = Limiter(
-    get_remote_address,  # Use the client's IP address to track the rate limit
-    app=app,
-    default_limits=["200 per day", "10 per minute"]  # Default rate limits
-)
+#limiter = Limiter(
+#    get_remote_address,  # Use the client's IP address to track the rate limit
+#    app=app,
+#    default_limits=["200 per day", "10 per minute"]  # Default rate limits
+#)
 
 #-------------------------------------------------------------------
 # app variables 
